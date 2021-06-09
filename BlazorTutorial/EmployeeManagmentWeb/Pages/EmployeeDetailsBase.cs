3f0bd8cca4626070c75a2bd3be1eff275de8fef5
DESKTOP-3FGF5CD\Katja <trynovak@gmail.com>
2021-06-09 20:35:04 +0300

    Blazor data binding 25

--- BlazorTutorial/EmployeeManagmentWeb/Pages/EmployeeDetailsBase.cs
+++ BlazorTutorial/EmployeeManagmentWeb/Pages/EmployeeDetailsBase.cs
@@ -13,7 +13,10 @@ namespace EmployeeManagmentWeb.Pages
     {
         public Employee Employee { get; set; } = new Employee();
 
-        protected string Coordinates { get; set }
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
 
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
@@ -21,15 +24,30 @@ namespace EmployeeManagmentWeb.Pages
         [Parameter]
         public string Id { get; set; }
 
-        protected  async override Task OnInitializedAsync()
+        protected async override Task OnInitializedAsync()
         {
             Id = Id ?? "1";
-            Employee =  await EmployeeService.GetEmployee(int.Parse(Id));
-            }
-
-        protected void Mouse_Move(MouseEventArgs e)
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+        protected void Button_Click()
         {
-            Coordinates = $"X = {e.ClientX } Y = {e.ClientY}";
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
-        }
+
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Coordinates = $"X = {e.ClientX } Y = {e.ClientY}";
+        // }
+        // }
     }
+}
+
