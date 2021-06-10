67ff2895e141f636b3573fb9227db64d3dbc3dfc
DESKTOP-3FGF5CD\Katja <trynovak@gmail.com>
2021-06-10 23:42:34 +0300

    Validating nested complex models and collection types in Blazor 38

--- BlazorTutorial/EmployeeManagmentWeb/Models/EditEmployeeModel.cs
+++ BlazorTutorial/EmployeeManagmentWeb/Models/EditEmployeeModel.cs
@@ -21,11 +21,14 @@ namespace EmployeeManagmentWeb.Models
             [EmailDomainValidator(AllowedDomain = "pragimtech.com",
                 ErrorMessage = "Only PragimTech.com is allowed")]
             public string Email { get; set; }
+            [CompareProperty("Email", ErrorMessage = "Email and Confirm Email must match")]
+            public string ConfirmEmail { get; set; }
             public DateTime DateOfBrith { get; set; }
             public Gender Gender { get; set; }
             public int DepartmentId { get; set; }
             public string PhotoPath { get; set; }
-            public Department Department { get; set; }
+        [ValidateComplexType]
+        public Department Department { get; set; } = new Department();
         }
     }
 
