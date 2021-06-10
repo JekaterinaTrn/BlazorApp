2c4058d456f29f1a44b5a86d47e50c0397b311be
DESKTOP-3FGF5CD\Katja <trynovak@gmail.com>
2021-06-11 00:20:43 +0300

    What is AutoMapper and using it in ASP NET Core 39

---
+++ BlazorTutorial/EmployeeManagmentWeb/Models/EmployeeProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoMapper;
+using EmployeeManagmentModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagmentWeb.Models
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee, EditEmployeeModel>()
+            .ForMember(dest => dest.ConfirmEmail,
+                      opt => opt.MapFrom(src => src.Email));
+            CreateMap< EditEmployeeModel, Employee>();
+        }
+    }
+}
--- BlazorTutorial/EmployeeManagmentWeb/Pages/EditEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagmentWeb/Pages/EditEmployeeBase.cs
@@ -1,4 +1,5 @@
-﻿using EmployeeManagmentModels;
+﻿using AutoMapper;
+using EmployeeManagmentModels;
 using EmployeeManagmentWeb.Models;
 using EmployeeManagmentWeb.Services;
 using Microsoft.AspNetCore.Components;
@@ -28,21 +29,26 @@ namespace EmployeeManagmentWeb.Pages
         [Parameter]
         public string  Id { get; set; }
 
+        [Inject]
+        public IMapper Mapper { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
             Departments = (await DepartmentService.GetDepartments()).ToList();
 
-            EditEmployeeModel.EmployeeId = Employee.EmployeeId;
-            EditEmployeeModel.FirstName = Employee.FirstName;
-            EditEmployeeModel.LastName = Employee.LastName;
-            EditEmployeeModel.Email = Employee.Email;
-            EditEmployeeModel.ConfirmEmail = Employee.ConfirmEmail;
-            EditEmployeeModel.DateOfBrith = Employee.DateOfBrith;
-            EditEmployeeModel.Gender = Employee.Gender;
-            EditEmployeeModel.PhotoPath = Employee.PhotoPath; 
-            EditEmployeeModel.DepartmentId = Employee.DepartmentId;
-            EditEmployeeModel.Department = Employee.Department;
+            Mapper.Map(Employee, EditEmployeeModel);
+
+            //EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            //EditEmployeeModel.FirstName = Employee.FirstName;
+            //EditEmployeeModel.LastName = Employee.LastName;
+            //EditEmployeeModel.Email = Employee.Email;
+            //EditEmployeeModel.ConfirmEmail = Employee.ConfirmEmail;
+            //EditEmployeeModel.DateOfBrith = Employee.DateOfBrith;
+            //EditEmployeeModel.Gender = Employee.Gender;
+            //EditEmployeeModel.PhotoPath = Employee.PhotoPath; 
+            //EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            //EditEmployeeModel.Department = Employee.Department;
 
         }
 
